797fbe9c37d47fc721b65f6d12e7e28527ca5738
Nick <nmarks99@gmail.com>
2022-02-01 13:24:36 -0600

    update

--- proto/stop_sign/main.py
+++ proto/stop_sign/main.py
@@ -10,17 +10,17 @@ t0 = time.time()
 
 # Load the cascade
 print('Analyzing image...')
-face_cascade = cv2.CascadeClassifier('stop_data.xml')
+cascade = cv2.CascadeClassifier('stop_data.xml')
 
 # Read the image
 img = cv2.imread(img_path)
 
-# Detect faces
-faces = face_cascade.detectMultiScale(img, 1.1, 4)
+# Detect desired image 
+detected = cascade.detectMultiScale(img, 1.1, 4)
 
 # Draw rectangles around faces
-for (x,y,w,h) in faces:
-    cv2.rectangle(img,(x,y), (x+w,y+h), (0,0,255), 2)
+for (x,y,w,h) in detected:
+    cv2.rectangle(img,(x,y), (x+w,y+h), (255,0,0), 5)
 
 # Save the result 
 cv2.imwrite('out.png',img)
