f80b10be991c5b19a47b3c97c20ff49f06cfa1bf
Nick <nmarks99@gmail.com>
2022-04-12 13:41:39 -0500

    save

---
+++ dronekit/dronekit_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import dronekit
+import time 
+
+def connect_drone(CONNECTION_STRING,BAUDRATE=115200):
+    print("Connecting...")
+    try:
+        vehicle = dronekit.connect(CONNECTION_STRING,BAUDRATE,wait_ready=True)
+    except:
+        print("Failed to connect")
+    print("Connection Established")
+    return vehicle
+
+
+# Close vehicle object before exiting script
+#  vehicle.close()
