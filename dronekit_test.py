855949a3d203a82e747181db085f8c0ba44e3cfa
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2022-04-12 12:26:31 -0500

    add dronekit_test.py

---
+++ dronekit_test.py
@@ -0,0 +1,25 @@
+from dronekit import connect, VehicleMode
+import time 
+
+print('begin')
+vehicle = connect("com3", baud=115200, wait_ready=True)
+print('connected')
+
+
+print(vehicle.version)
+print(vehicle.is_armed)
+
+# arm the vehicle
+# vehicle.armed = True
+
+
+
+# while not vehicle.armed:
+#     print("Trying to arm")
+#     time.sleep(0.2)
+
+
+
+
+# Close vehicle object before exiting script
+vehicle.close()
