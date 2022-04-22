504cbbe3f9e07294e7c486a32fa167a0fd70f1bc
Nick <nmarks99@gmail.com>
2022-04-22 13:39:25 -0500

    edit main.py

--- dronekit/main.py
+++ dronekit/main.py
@@ -7,6 +7,7 @@ main script for running dronekit
 
 import dronekit
 import dklib
+import time
 from dklib import clear_print
 
 # Define connection port and baudrate
@@ -19,7 +20,15 @@ vehicle = dronekit.connect(PORT, baud=BAUD, wait_ready=True)
 clear_print("Connected Successfully!\n\n")
 
 # Set the servo position
+dklib.set_servo(vehicle, 9, "low")
+time.sleep(1)
 dklib.set_servo(vehicle, 9, "mid")
+time.sleep(1)
+dklib.set_servo(vehicle, 9, "high")
+
+while True:
+    print(vehicle.attitude)
+    time.sleep(0.2)
 
 # Close vehicle object
 vehicle.close()
