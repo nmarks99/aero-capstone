e70e0f79f511ca6709e246cfe444b189d26311ea
Nick <nmarks99@gmail.com>
2022-05-09 20:22:45 -0500

    add imu_test.py

--- imu_test.py
+++ imu_test.py
@@ -1,11 +1,8 @@
+import board
+import busio
 import adafruit_bno055
 
-import board 
-i2c = board.I2C()
-
+i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_bno055.BNO055_I2C(i2c)
 
 print(sensor.temperature)
-print(sensor.euler)
-print(sensor.gravity)
-
