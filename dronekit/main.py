77238cd5de2c1cc82f8ef0a6a9171425a68a5945
Nick <nmarks99@gmail.com>
2022-05-13 14:19:46 -0500

    update main.py with full mission program

--- dronekit/main.py
+++ dronekit/main.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
-
-'''
-main script for running dronekit
-'''
-
 import dronekit
 import dklib
 import time
+import imu
 from dklib import clear_print
+
+"""
+SETUP
+"""
 
 # Define connection port and baudrate
 PORT = "/dev/serial0" # Serial port on the Pi
@@ -20,26 +20,68 @@ vehicle = dronekit.connect(PORT, baud=BAUD, wait_ready=True)
 clear_print("Connected Successfully!\n\n")
 input("Press enter to continue")
 
+# Connect IMU
+IMU = imu.connect_imu()
+
 # Arm the vehicle
 while not vehicle.armed:
     clear_print("Waiting to arm...")
     vehicle.armed = True
     time.sleep(1)
+    print("Armed!")
 
-# Takeoff and go to a target altitude
-print("Taking off!")
-TARGET_ALT = 3 # meters
-dklib.takeoff(vehicle, TARGET_ALT)
 
-# Hover at this spot for 
-hover_time = 3 # 3 seconds 
-dklib.set_attitude(duration=hover_time)
+"""
+MAIN PROGRAM LOOP
+"""
+acc_data = [] 
+DROP_THRESHOLD = 1234 # TODO: update this
+HOVER_THRESHOLD = 1234
+ARM_SERVO = 9
+LEG_SERVO = 10 # TODO: check number
+t0 = time.time()
 
-# Land 
-print("Landing...")
+# Pre-drop loop
+while True:
+    t_now = time.time() - t0
+    
+    ax = IMU.acceleration[0]
+    ay = IMU.acceleration[1]
+    az = IMU.acceleration[2]
+    acc_data.append((ax,ay,az), t_now)
+    
+    if az >= DROP_THRESHOLD:
+        clear_print("Drop detected!...Deploying arms")
+        dklib.set_servo(vehicle, ARM_SERVO, "HIGH")
+        time.sleep(0.1) # TODO: check this sleep
+        break
+
+    time.sleep(0.05) # TODO: is this a good delay?
+
+
+# After drop
+while True:
+    
+    t_now = time.time() - t0
+   
+    ax = IMU.acceleration[0]
+    ay = IMU.acceleration[1]
+    az = IMU.acceleration[2]
+    acc_data.append((ax,ay,az), t_now)
+    
+    # Set thrust to max
+    dklib.set_attitude(thrust=1.0)
+    time.sleep(0.05)
+    
+    if az <= HOVER_THRESHOLD:
+        clear_print("Hover reached!")
+        dklib.set_servo(vehicle, LEG_SERVO, "HIGH")
+        break
+    
 vehicle.mode = dronekit.VehicleMode("LAND")
-time.sleep(1)
+input("Press enter when complete")
+
+# TODO: Add code to save acceleration data
 
 # Close vehicle object
 vehicle.close()
-clear_print("Mission complete")
