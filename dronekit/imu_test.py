a633f3d6e93e1fd5b64f028b84b3e199a67bf3b9
Nick <nmarks99@gmail.com>
2022-05-13 15:49:01 -0500

    save

--- dronekit/imu_test.py
+++ dronekit/imu_test.py
@@ -6,15 +6,17 @@ IMU = imu.connect_imu()
 
 data = []
 t0 = time.time()
-while True:
-    t_now = time.time() - t0
-    ax = IMU.acceleration[0]
-    ay = IMU.acceleration[1]
-    az = IMU.acceleration[3]
-    data.append([ax,ay,az],t_now)
-    time.sleep(0.05)
-
-imu.write_to_file(data)
+try:
+    while True:
+        t_now = time.time() - t0
+        ax = IMU.acceleration[0]
+        ay = IMU.acceleration[1]
+        az = IMU.acceleration[3]
+        data.append([ax,ay,az],t_now)
+        time.sleep(0.05)
+except KeyboardInterrupt:
+    imu.write_to_file(data)
+    exit()
 
 
 
