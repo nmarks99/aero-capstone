2d43e901e3e6863d04250de5e21565c0859df431
Nick <nmarks99@gmail.com>
2022-05-12 20:20:49 -0500

    save

---
+++ dronekit/imu.py
@@ -0,0 +1,10 @@
+import board
+import busio
+import adafruit_bno055
+import time
+from dklib import clear_print
+
+def connect_imu():
+    i2c = busio.I2C(board.SCL, board.SDA)
+    imu = adafruit_bno055.BNO055_I2C(i2c)
+    return imu
