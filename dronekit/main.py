93c295a8231dacae1db32da2d6a9788257d78865
Nick <nmarks99@gmail.com>
2022-05-14 12:34:28 -0500

    save

--- dronekit/main.py
+++ dronekit/main.py
@@ -31,13 +31,10 @@ while not vehicle.armed:
     print("Armed!")
 
 
-
-
-
 """
 MAIN PROGRAM LOOP
 """
-acc_data = [] 
+# Define constants
 DROPPED = False # Flag for if drop has been detected or not
 DROP_THRESHOLD = 1234 # TODO: update thresholds
 HOVER_THRESHOLD = 1234
@@ -45,51 +42,64 @@ ARM_SERVO = 9
 LEG_SERVO = 10 # TODO: check servo number
 
 t0 = time.time() # time = 0 here
+acc_data = [] # array to store acceleration data 
 
-# Waiting for drop to be detected 
-while True:
-    t_now = time.time() - t0
-    
-    ax = IMU.acceleration[0]
-    ay = IMU.acceleration[1]
-    az = IMU.acceleration[2]
-    acc_data.append((ax,ay,az), t_now)
-    
-    if az >= DROP_THRESHOLD:
-        clear_print("Drop detected!...Deploying arms")
-        dklib.set_servo(vehicle, ARM_SERVO, "HIGH")
-        time.sleep(0.1) # TODO: check this sleep
-        break
+try:
+    while True:
+        
+        # Get current time and acceleration
+        t_now = time.time() - t0
+        (ax, ay, az, amag) = imu.read_acc(IMU)
+        acc_data.append([ax, ay, az, amag, t_now]) 
 
-    time.sleep(0.05) # TODO: is this a good delay?
+        # Print out acceleration data
+        print(
+            "ax = {:.3f}\tay = {:.3f}\taz = {:.3f}\tamag = {:.3f}\tt = {:.3f} s"
+            .format(ax,ay,az,amag,t_now)
+        )
+        
 
+        if not DROPPED:
+            # Check if drop detected
+            if amag >= DROP_THRESHOLD:
+                DROPPED = True
+                 
+                # Deploy arms
+                print("Arms deployed")
+                dklib.set_servo(vehicle, ARM_SERVO, "HIGH")
+                
+                # Set throttle to 100%
+                print("Throttle set to 100%")
+                dklib.set_attitude(thrust=1.0)
+        
+        # Check if hover detected
+        elif DROPPED:
+            # Keep throttle at max until hover is detected
+            dklib.set_attitude(thrust=1.0)
+            
+            if amag <= HOVER_THRESHOLD:
+                print("Hover achieved!")
 
-# Drop detected, waiting to reach hover
-while True:
-    
-    t_now = time.time() - t0
-   
-    ax = IMU.acceleration[0]
-    ay = IMU.acceleration[1]
-    az = IMU.acceleration[2]
-    acc_data.append((ax,ay,az), t_now)
-    
-    # Set thrust to max
-    dklib.set_attitude(thrust=1.0)
-    
-    if az <= HOVER_THRESHOLD:
-        clear_print("Hover reached!")
-        dklib.set_servo(vehicle, LEG_SERVO, "HIGH")
-        break
-    
-    time.sleep(0.05)
-   
-# Set to land mode 
-vehicle.mode = dronekit.VehicleMode("LAND")
-input("Press enter when complete")
+                # Deploy legs
+                # Hold position for 2 seconds
+                print("Legs deployed")
+                dklib.set_servo(vehicle, LEG_SERVO, "HIGH")
+                dklib.set_attitude(duration=2) # TODO: Check if this works
+                
+                # Set to LAND mode
+                vehicle.mode = dronekit.VehicleMode("LAND")
+                break # Break out of the loop when we switch to LAND
 
-# Save acceleration data
-imu.write_to_file(acc_data)
+        time.sleep(0.05) # TODO: check if this delay is sufficient
 
-# Close vehicle object
-vehicle.close()
+    time.sleep(10) # TODO: Check the time here
+    print("Mission Complete")
+    
+    # Close vehicle object
+    vehicle.close()
+
+except KeyboardInterrupt:
+    # Write the acceleration data to a file
+    imu.write_to_file(acc_data)
+    pass # TODO: add code to safely abort mission
+
