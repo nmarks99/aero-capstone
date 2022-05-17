76713cac563b3796dcf2c8184df68c7367114795
Nick <nmarks99@gmail.com>
2022-05-16 22:36:34 -0500

    save

--- dronekit/simple.py
+++ dronekit/simple.py
@@ -6,9 +6,32 @@ import dklib
 import time
 import imu
 from utils import clear_print, color_print
-
+import threading
 
 def main():
+
+    # Connect IMU and make a thread function
+    IMU = imu.connect()
+    t0 = time.time()
+    def imu_thread_func(data_arr):
+        while True:
+            t = time.time() - t0
+            ax,ay,az,amag = imu.read_acc(IMU)
+            
+            # Print out acceleration data
+            print(
+                "ax = {:.3f}\tay = {:.3f}\taz = {:.3f}\tamag = {:.3f}\tt = {:.3f} s"
+                .format(ax,ay,az,amag,t)
+            )
+            data_arr.append([ax,ay,az,amag,t])
+            global stop_thread
+            if stop_thread:
+                break
+
+    stop_thread = False
+    data_arr = []
+    imu_thread = threading.Thread(target=imu_thread_func,args=(data_arr,))
+    imu_thread.start()
 
     try:
         # Define connection port and baudrate
@@ -36,13 +59,14 @@ def main():
 
         # Take off in GUIDED_NOGPS mode.
         color_print("Taking off...","BOLD_RED")
-        TAKEOFF_ALTITUDE = 3
+        TAKEOFF_ALTITUDE = 2
         dklib.takeoff(vehicle,target_altitude=TAKEOFF_ALTITUDE)
 
         # Hold the position for 3 seconds.
         print("Holding position for 3 seconds")
         dklib.set_attitude(vehicle,duration = 3)
-
+        time.sleep(1)
+        
         # Land
         vehicle.mode = dronekit.VehicleMode("LAND")
 
@@ -53,8 +77,15 @@ def main():
 
 
     except KeyboardInterrupt:
+        color_print("PANIC! ABORT MISSION")
         vehicle.mode = dronekit.VehicleMode("LAND")
+        time.sleep(2)
+        while True:
+            dklib.set_attitude(thrust=0.0)
+            time.sleep(1)
 
+    stop_thread = True
+    imu_thread.join()
 
 
 if __name__ == "__main__":
