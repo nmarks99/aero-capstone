8fae9b2df02f2075b770bbc3298f528fcd589f65
Nick <nmarks99@gmail.com>
2022-05-16 22:01:13 -0500

    add kill switch to simple.py

--- dronekit/simple.py
+++ dronekit/simple.py
@@ -10,49 +10,49 @@ from utils import clear_print, color_print
 
 def main():
 
+    try:
+        # Define connection port and baudrate
+        PORT = "/dev/serial0" # Serial port on the Pi
+        BAUD = 921600
 
-    # Define connection port and baudrate
-    PORT = "/dev/serial0" # Serial port on the Pi
-    BAUD = 921600
+        # Connect to the Pixhawk
+        clear_print("Connecting...")
+        vehicle = dronekit.connect(PORT, baud=BAUD, wait_ready=True)
+        color_print("Connected Successfully!\n\n","BOLD_RED")
+        input("Press enter to continue")
 
-    # Connect to the Pixhawk
-    clear_print("Connecting...")
-    vehicle = dronekit.connect(PORT, baud=BAUD, wait_ready=True)
-    color_print("Connected Successfully!\n\n","BOLD_RED")
-    input("Press enter to continue")
+        # Connect IMU
+        IMU = imu.connect_imu()
 
-    # Connect IMU
-    IMU = imu.connect_imu()
+        # Set vehicle mode to GUIDED_NOGPS
+        vehicle.mode = dronekit.VehicleMode("GUIDED_NOGPS")
 
-    # Set vehicle mode to GUIDED_NOGPS
-    vehicle.mode = dronekit.VehicleMode("GUIDED_NOGPS")
+        # Arm the vehicle
+        while not vehicle.armed:
+            clear_print("Waiting to arm...")
+            vehicle.armed = True
+            time.sleep(1)
+            color_print("Armed","BOLD_RED")
 
-    # Arm the vehicle
-    while not vehicle.armed:
-        clear_print("Waiting to arm...")
-        vehicle.armed = True
-        time.sleep(1)
-        color_print("Armed","BOLD_RED")
+        # Take off in GUIDED_NOGPS mode.
+        color_print("Taking off...","BOLD_RED")
+        TAKEOFF_ALTITUDE = 3
+        dklib.takeoff(vehicle,target_altitude=TAKEOFF_ALTITUDE)
 
-    input("Press enter to continue")
+        # Hold the position for 3 seconds.
+        print("Holding position for 3 seconds")
+        dklib.set_attitude(vehicle,duration = 3)
 
-    # Take off in GUIDED_NOGPS mode.
-    color_print("Taking off...","BOLD_RED")
-    TAKEOFF_ALTITUDE = 3
-    dklib.takeoff(vehicle,target_altitude=TAKEOFF_ALTITUDE)
+        # Land
+        vehicle.mode = dronekit.VehicleMode("LAND")
 
-    # Hold the position for 3 seconds.
-    print("Holding position for 3 seconds")
-    dklib.set_attitude(vehicle,duration = 3)
+        # Close when user is ready
+        color_print("Mission Complete. Press enter to close vehicle", "BOLD_GREEN")
+        input("")
+        vehicle.close()
 
-    # Land
-    vehicle.mode = dronekit.VehicleMode("LAND")
-
-    # Close when user is ready
-    color_print("Mission Complete. Press enter to close vehicle", "BOLD_GREEN")
-    input("")
-    vehicle.close()
-
+    except KeyboardInterrupt:
+        vehicle.mode = dronekit.VehicleMode("LAND")
 
 if __name__ == "__main__":
     main()
