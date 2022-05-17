73fd0a3d880a27319a08c4f5661c7b23d3f3272c
Nick <nmarks99@gmail.com>
2022-05-17 00:48:13 -0500

    create threading stuff

--- dronekit/simple.py
+++ dronekit/simple.py
@@ -8,52 +8,34 @@ import imu
 from utils import clear_print, color_print
 import threading
 
+
+
 def main():
     
-    stop_thread = False
-    # Connect IMU and make a thread function
-    IMU = imu.connect()
-    t0 = time.time()
-    def imu_thread_func(data_arr):
-        while True:
-            t = time.time() - t0
-            ax,ay,az,amag = imu.read_acc(IMU)
-            
-            # Print out acceleration data
-            print(
-                "ax = {:.3f}\tay = {:.3f}\taz = {:.3f}\tamag = {:.3f}\tt = {:.3f} s"
-                .format(ax,ay,az,amag,t)
-            )
-            data_arr.append([ax,ay,az,amag,t])
-            global stop_thread
-            if stop_thread:
-                break
-
-    stop_thread = False
-    data_arr = []
-    imu_thread = threading.Thread(target=imu_thread_func,args=(data_arr,))
-    imu_thread.start()
-
     try:
         # Define connection port and baudrate
         PORT = "/dev/serial0" # Serial port on the Pi
         BAUD = 921600
 
         # Connect to the Pixhawk
-        clear_print("Connecting...")
+        color_print("Connecting...", "BOLD_RED",clear=True)
         vehicle = dronekit.connect(PORT, baud=BAUD, wait_ready=True)
         color_print("Connected Successfully!\n\n","BOLD_RED")
         input("Press enter to continue")
 
-        # Connect IMU
-        #  IMU = imu.connect_imu()
+        # Read IMU data in a separate thread and store it in a list
+        # Format is [[ax,ay,az,amag,timestamp]]
+        stop_thread = False
+        data_arr = []
+        imu_thread = threading.Thread(target=imu.imu_thread_func,args=(data_arr, lambda : stop_thread,))
+        imu_thread.start()
 
         # Set vehicle mode to GUIDED_NOGPS
         vehicle.mode = dronekit.VehicleMode("GUIDED_NOGPS")
 
         # Arm the vehicle
         while not vehicle.armed:
-            clear_print("Waiting to arm...")
+            color_print("Waiting to arm...","BOLD_RED",clear=True)
             vehicle.armed = True
             time.sleep(1)
             color_print("Armed","BOLD_RED")
@@ -66,7 +48,6 @@ def main():
         # Hold the position for 3 seconds.
         print("Holding position for 3 seconds")
         dklib.set_attitude(vehicle,duration = 3)
-        time.sleep(1)
         
         # Land
         vehicle.mode = dronekit.VehicleMode("LAND")
@@ -84,9 +65,11 @@ def main():
         while True:
             dklib.set_attitude(thrust=0.0)
             time.sleep(1)
-
+    
+    # Stop the thread and write the IMU data to a text file
     stop_thread = True
     imu_thread.join()
+    imu.write_to_file(data_arr)
 
 
 if __name__ == "__main__":
