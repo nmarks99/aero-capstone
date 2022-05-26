4b0f78b42531ab3b9b6b6d575f03e779446ed915
Nick <nmarks99@gmail.com>
2022-05-26 10:43:17 -0500

    save

--- dronekit/imu_test.py
+++ dronekit/imu_test.py
@@ -1,25 +1,44 @@
 #!/usr/bin/env python3
+import utils
 import imu
 import time
 from math import sqrt
+import threading
+#
+#  IMU = imu.connect_imu()
+#
+#  data = []
+#  t0 = time.time()
+#  try:
+    #  while True:
+        #  t_now = time.time() - t0
+        #  ax = IMU.acceleration[0]
+        #  ay = IMU.acceleration[1]
+        #  az = IMU.acceleration[3]
+        #  amag = sqrt(ax**2 + ay**2 + az**2)
+        #  data.append([ax,ay,az,amag,t_now])
+        #  print(ax, ay, az,amag,t_now)
+        #  time.sleep(0.05)
+#  except KeyboardInterrupt:
+    #  imu.write_to_file(data)
+    #  exit()
+#
+#
 
-IMU = imu.connect_imu()
+stop_thread = threading.event()
+acc_data = []
+imu_thread = threading.Thread(target=imu.imu_thread_func,args=(acc_data,stop_thread,))
+imu_thread.start()
 
-data = []
-t0 = time.time()
 try:
     while True:
-        t_now = time.time() - t0
-        ax = IMU.acceleration[0]
-        ay = IMU.acceleration[1]
-        az = IMU.acceleration[3]
-        amag = sqrt(ax**2 + ay**2 + az**2)
-        data.append([ax,ay,az,amag,t_now])
-        print(ax, ay, az,amag,t_now)
-        time.sleep(0.05)
+        time.sleep(0.5)
+        print(len(acc_data))
 except KeyboardInterrupt:
-    imu.write_to_file(data)
-    exit()
+    stop_thread.set()
+    print(acc_data)
+
+
 
 
 
