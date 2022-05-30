248f3ce986560bc707d3e663ed7197890f03e570
Nick <nmarks99@gmail.com>
2022-05-30 18:25:49 -0500

    save

--- dronekit/main.py
+++ dronekit/main.py
@@ -25,23 +25,12 @@ def main():
     brint("Connected Successfully!\n\n",color="BOLD_RED")
     input("Press enter to continue")
 
-    # Set vehicle mode to GUIDED_NOGPS
-    vehicle.mode = dronekit.VehicleMode("GUIDED_NOGPS")
-
-    # Arm the vehicle
-    while not vehicle.armed:
-        brint("Waiting to arm...",clear=True)
-        vehicle.armed = True
-        time.sleep(1)
-        brint("Armed",color="BOLD_RED")
-
     # Read IMU data in a separate thread and store it in a list
     # Format is [[ax,ay,az,amag,timestamp]]
     stop_thread = threading.Event()
     acc_data = []
     imu_thread = threading.Thread(target=imu.imu_thread_func,args=(acc_data,stop_thread,))
     imu_thread.start()
-
 
     """
     MAIN PROGRAM LOOP
@@ -52,108 +41,66 @@ def main():
     HOVER_THRESHOLD = 8.8
     ARM_SERVO = 9
     LEG_SERVO = 10 # TODO: check servo number
-
-    # Take off in GUIDED_NOGPS mode.
-    print("Taking off...")
-    TAKEOFF_ALTITUDE = 5
-    dklib.takeoff(vehicle,target_altitude=TAKEOFF_ALTITUDE)
-
-    # Hold the position for 3 seconds.
-    print("Holding position for 3 seconds")
-    dklib.set_attitude(duration = 3)
-
-    # Cut motors
-    print("Cut motors")
-    dklib.set_attitude(thrust=0.0)
-    time.sleep(0.2)
-
+    
     try:
         
         while True:
             
             # Get acceleration magnitude from the acc_data array
             # last list is the most recent since its running in parallel 
-            if len(acc_data) > 0:
+            if len(acc_data) > 0: # wait until we have data
+
                 ax = round(acc_data[-1][0],4)
                 ay = round(acc_data[-1][1],4)
                 az = round(acc_data[-1][2],4)
                 amag = round(acc_data[-1][3],4)
                 t = round(acc_data[-1][4],4)
                 
-                # Print out acceleration data
+
+                if not DROPPED:
+                    
+                    # Print out acceleration data
+                    brint("",clear=True)
+                    print("Dropped = ",end="")
+                    brint("False",color="BOLD_RED") 
+
+                    # Check if drop detected
+                    if amag >= DROP_THRESHOLD:
+                        DROPPED = True
+                        
+                        # Deploy arms
+                        dklib.set_servo(vehicle, ARM_SERVO, "HIGH")
+    
+                        # Deploy legs
+                        dklib.set_servo(vehicle, LEG_SERVO, "HIGH")
+                
+                        brint("",clear=True)
+                        print("Dropped = ",end="")
+                        brint("True",color="BOLD_GREEN")
+                        break
+
                 print(
                     "ax = {:.3f}\tay = {:.3f}\taz = {:.3f}\tamag = {:.3f}\tt = {:.3f} s"
                     .format(ax,ay,az,amag,t)
                 )
-
-                    
-
-            if not DROPPED:
-                # Check if drop detected
-                if amag >= DROP_THRESHOLD:
-                    DROPPED = True
-                    
-                    # Deploy arms
-                    brint("Arms deployed",color="BOLD_RED")
-                    dklib.set_servo(vehicle, ARM_SERVO, "HIGH")
-                    
-                    # Set throttle to 100%
-                    brint("Throttle set to 100%",color="BOLD_RED")
-                    dklib.set_attitude(thrust=1.0)
-
-                elif vehicle.location.global_relative_frame.alt <= 3.0:
-                    brint("PANIC! ABORT MISSION!",color="BOLD_RED")
-                    dklib.set_attitude(thrust=1.0)
-                    time.sleep(0.5)
-                    vehicle.mode = dronekit.VehicleMode("ALT_HOLD")
-            
-            # Check if hover detected
-            elif DROPPED:
-                # Keep throttle at max until hover is detected
-                dklib.set_attitude(thrust=1.0)
                 
-                if amag <= HOVER_THRESHOLD or vehicle.location.global_relative_frame.alt >= TAKEOFF_ALTITUDE:
-                    brint("Hover achieved",color="BOLD_RED")
+                time.sleep(0.05) # TODO: check if this delay is sufficient
 
-                    # Deploy legs
-                    # Hold position for 2 seconds
-                    brint("Legs deployed",color="BOLD_RED")
-                    dklib.set_servo(vehicle, LEG_SERVO, "HIGH")
 
-                    # Hold altitude here
-                    vehicle.mode = dronekit.VehicleMode("ALT_HOLD")
-                    time.sleep(5)
-                    
-                    # Set to LAND mode
-                    vehicle.mode = dronekit.VehicleMode("LAND")
-                    break # Break out of the loop when we switch to LAND
-
-            time.sleep(0.05) # TODO: check if this delay is sufficient
-
-        imu.write_to_file(acc_data) # save imu data
-        time.sleep(5) # TODO: Check the time here
         brint("Mission Complete",color="BOLD_GREEN")
         
         # Close vehicle object
         vehicle.close()
 
-
     except KeyboardInterrupt:
-        brint("KEYBOARD INTERRUPT\nABORTING MISSION\nLANDING...",color="BOLD_RED")
+        brint("KEYBOARD INTERRUPT\nABORTING MISSION",color="BOLD_RED")
         
-        # Set vehicle to land
-        vehicle.mode = dronekit.VehicleMode("LAND")
-        time.sleep(5)
-
-        # Write the acceleration data to a file
-        imu.write_to_file(acc_data)
-
         # Close vehicle object
         vehicle.close()
 
 
     # Stop the thread and write the IMU data to a text file
-    stop_thread = True
+    stop_thread.set()
     imu_thread.join()
     imu.write_to_file(acc_data)
 
