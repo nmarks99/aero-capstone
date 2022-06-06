7505a85bb21b64dad878f5f4d85ed24781e5bba9
Nick <nmarks99@gmail.com>
2022-06-06 08:38:18 -0500

    update outfile

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ def gen_unique_filename(default_name,extension, directory="./"):
     name already exists, the generated string will be "default_name_1.extension". If that 
     already exists, the string will be "default_name_2.extension" and so on.
     '''
+     
     contents = os.listdir(directory)
     nums = []
     f = False
@@ -20,13 +21,13 @@ def gen_unique_filename(default_name,extension, directory="./"):
             nums.sort()
 
     if not f:
-        outfile = "".join([default_name,extension])
+        outfile = "".join([directory,default_name,extension])
     else:
         if len(nums) >= 1:
             n = nums[-1]
-            outfile = "".join([default_name,"_{}".format(n+1),extension])
+            outfile = "".join([directory,default_name,"_{}".format(n+1),extension])
         else:
-            outfile = "".join([default_name,"_1",extension])
+            outfile = "".join([directory,default_name,"_1",extension])
 
     return outfile
 
