8c4bd58518e511ea206fbc75da1216acf8e46e30
Your Name <nicholasmarks2022@u.northwestern.edu>
2022-06-06 01:20:31 +0100

    SAve

--- dronekit/servo_test.py
+++ dronekit/servo_test.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 import dklib
 import dronekit
+import time
+
 
 v = dronekit.connect("/dev/serial0",baud=921600,wait_ready=True)
 
 input("Press enter to spin servo")
 dklib.set_servo(v, 9, "LOW")
+time.sleep(1)
+dklib.set_servo(v, 9, "HIGH")
+time.sleep(5)
 
 
 
