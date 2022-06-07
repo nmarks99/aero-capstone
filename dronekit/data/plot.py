4c1d2f652613949d0361fcedf17da8a0c834546e
Nick <nmarks99@gmail.com>
2022-06-07 09:04:03 -0500

    save

--- dronekit/data/plot.py
+++ dronekit/data/plot.py
@@ -1,14 +1,21 @@
 from matplotlib import pyplot as plt
+import numpy as np
 
 amag = []
 t = []
-with open("data/imu_data_1.txt","r") as f:
+with open("imu_data_1.txt","r") as f:
     for row in f:
         row = row.split(',')
         amag.append(float(row[3]))
         t.append(float(row[4]))
 
+
+
 fig, ax = plt.subplots()
-ax.plot(t,amag)
+ax.plot(t,amag,'-b',"Data")
+ax.grid()
+hline = [8.0 for i in range(len(t))]
+ax.plot(t,hline,'--r',label="Threshold")
+ax.legend()
 plt.show()
         
