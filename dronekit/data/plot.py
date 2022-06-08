8956c1cb35a77c34c325a06dfa2d57073e974605
Nick <nmarks99@gmail.com>
2022-06-08 09:39:41 -0500

    save

--- dronekit/data/plot.py
+++ dronekit/data/plot.py
@@ -1,21 +1,25 @@
+#!/usr/bin/env python3
 from matplotlib import pyplot as plt
 import numpy as np
 
+data_path = "imu_data_3.txt"
+
 amag = []
 t = []
-with open("imu_data_1.txt","r") as f:
+with open(data_path,"r") as f:
     for row in f:
         row = row.split(',')
         amag.append(float(row[3]))
         t.append(float(row[4]))
 
 
-
+plt.style.use("ggplot")
 fig, ax = plt.subplots()
-ax.plot(t,amag,'-b',"Data")
-ax.grid()
+ax.plot(t,amag,'-b',label="Data")
 hline = [8.0 for i in range(len(t))]
 ax.plot(t,hline,'--r',label="Threshold")
 ax.legend()
+ax.set(xlabel="Time (s)",ylabel="Acceleration ($m/s^2$)")
+#  plt.yticks(np.arange(0,35,2))
 plt.show()
-        
+fig.savefig("data3.png")        
